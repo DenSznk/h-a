8c99d1bd1be6760f7bfb80fed0b264c821498287
DenSznk <densznk@gmail.com>
2024-02-14 15:23:28 +0400

    added createUsers func

---
+++ ha2/Create Objects/script.js
@@ -0,0 +1,37 @@
+function createUsers(name, age) {
+  const result = [];
+  result.push(defProperties(name, age));
+  result.push(objCreate(name, age));
+  return result;
+}
+
+function defProperties(name, age) {
+  const obj = {};
+  Object.defineProperties(obj, {
+    name: {
+      value: name,
+      writable: true,
+      enumerable: true,
+    },
+    age: {
+      value: age,
+      writable: true,
+      enumerable: true,
+    },
+  });
+  return obj;
+}
+
+function objCreate(name, age) {
+  const obj = {
+    name,
+    age,
+  };
+
+  const user = Object.create(obj);
+  user.name = name;
+  user.age = age;
+  return user;
+}
+
+console.log(createUsers("John", 35));
