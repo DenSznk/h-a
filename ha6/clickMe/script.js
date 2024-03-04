e66303c5de4436bc7f72abc2bf8f53e0af03ecb0
DenSznk <densznk@gmail.com>
2024-03-04 10:51:28 +0400

    click button v1

--- ha6/clickMe/script.js
+++ ha6/clickMe/script.js
@@ -1,5 +1,20 @@
 const button = document.querySelector('.my-button');
 
-button.addEventListener('click', () => {
+function returnTrueOrFalse () {
+    let random = Math.random();
+    if (random < 0.5) {
+        return true
+    } else {
+        return false
+    }
+}
 
-});
+function moveButtonOnHover () {
+    if (returnTrueOrFalse) {
+        let randomX = Math.floor(Math.random() * window.innerWidth);
+        let randomY = Math.floor(Math.random() * window.innerHeight);
+        button.style.left = randomX + 'px';
+        button.style.top = randomY + 'px';
+    }
+}
+
