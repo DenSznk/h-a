2f7cc4f1ab619098a4abdf55c3403e668412ec1d
DenSznk <densznk@gmail.com>
2024-03-04 12:48:47 +0400

    finish the stack isIterable

--- ha5/stack/script.js
+++ ha5/stack/script.js
@@ -1,15 +1,18 @@
 class Stack {
   stack = [];
   constructor(maxElementsAmount = 10) {
-    if (typeof maxElementsAmount === "number" && !isNaN(maxElementsAmount) && isFinite(maxElementsAmount)) {
-        this.maxElementsAmount = maxElementsAmount;
-    } else throw new Error("Not correct value")
+    if (
+      typeof maxElementsAmount === "number" && !isNaN(maxElementsAmount) && isFinite(maxElementsAmount)) {
+      this.maxElementsAmount = maxElementsAmount;
+    } else throw new Error("Not correct value");
   }
+
   push(num) {
     if (this.stack.length < this.maxElementsAmount) {
       this.stack.push(num);
     } else throw new Error("The stack is full!");
   }
+
   pop() {
     if (this.stack.length !== 0) {
       const lastElement = this.stack.length - 1;
@@ -18,6 +21,7 @@ class Stack {
       throw new Error("The stack is already empty");
     }
   }
+
   peek() {
     if (this.stack.length !== 0) {
       const lastElement = this.stack.length - 1;
@@ -25,19 +29,48 @@ class Stack {
     } else return null;
   }
   toArray() {
-    return this.stack
+    return this.stack;
   }
-  isEmpty () {
-    return this.stack.length === 0
+  isEmpty() {
+    return this.stack.length === 0;
   }
-  fromIterable(iterable) {
-    const newStack = new Stack()
 
-    return newStack
+  static isIterable(entity) {
+    return typeof entity[Symbol.iterator] === "function";
   }
+//
+  fromIterable(entity) {
+    const newStack = new Stack();
+    if (!Stack.isIterable(entity)) {
+      throw new Error("Not iterable");
+    } else {
+      for (const item of entity) {
+        newStack.push(item);
+      }
+      return newStack;
+    }
+  }
+
+  // static fromIterable(iterable) {
+  //   const newStack = new Stack();
+  //   if (!Stack.isIterable(iterable)) {
+  //     throw new Error("Not iterable");
+  //   } else {
+  //     for (const item of iterable) {
+  //       newStack.push(item);
+  //     }
+  //     return newStack;
+  //   }
+  // }
 
 }
 
 const test = new Stack(12);
-console.log(test.isEmpty())
-console.log(test.maxElementsAmount);
+// console.log(test.isEmpty())
+// console.log(test.maxElementsAmount);
+
+// const arr = [1, 2, 3, 4, 5];
+// console.log(Stack.fromIterable(arr).toArray());
+
+const arr = [1, 2, 3, 4, 5];
+console.log(test.fromIterable(arr).toArray());
